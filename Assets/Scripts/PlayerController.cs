c516b138ad6dba529fbce64cb114139b43db6d79
Cole Horvat <ColeHorvat@gmail.com>
2023-02-02 20:24:33 -0800

    Player can now move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Vector2 _move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       MovePlayer(); 
+    }
+
+    public void OnMove(InputAction.CallbackContext context) 
+    {
+        _move = context.ReadValue<Vector2>();
+    }
+
+    public void MovePlayer()
+    {
+        Vector3 movement = new Vector3(_move.x, 0f, _move.y);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
